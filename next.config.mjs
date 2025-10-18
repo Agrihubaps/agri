f957d4291ca77dfc3363d3a104ffbc779d279359
Agrihubaps <agrihubaps@agrihubfarm.dk>
2025-10-18 15:41:59 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
+// next.config.mjs
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true }
-}
-export default nextConfig
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "lucide-react": path.resolve(process.cwd(), "lib/icon-shim.tsx"),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
